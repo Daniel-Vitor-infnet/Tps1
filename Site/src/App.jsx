9b80fe35941016a058e4ea936c2f34e149b25068
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2024-09-20 16:21:59 -0300

    update

--- Site/src/App.jsx
+++ Site/src/App.jsx
@@ -4,7 +4,15 @@ import LinkList from './componentes/LinkList';
 export default function App() {
   return (
     <main>
+      <header>
+        <h1>Fundamentos de React [24E3_2]</h1>
+        <p>Aluno: <strong>Daniel Vitor Madeira Reis</strong></p>
+        <p>Professor: <strong>Wesley Bruno</strong></p>
+        <p id='obs'>Observações: Cada link abrirá uma nova aba com o repositório. Devido a alguns problemas com o "Replit", estou utilizando o "codesandbox.io", conforme sugerido no chat do grupo, e acredito que não haverá problemas. </p>
+      </header>
+      
       <section>
+        <h2>Tarefas Práticas (TPs)</h2>
         <LinkList />
       </section>
     </main>
